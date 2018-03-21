35bd7186c4cca851a3c4e541ae0beb3bf674668f
AlexanderBreki <35424359+AlexanderBreki@users.noreply.github.com>
2018-03-21 14:15:23 +0000

    ABJ

--- Sprint 3/Thing.py
+++ Sprint 3/Thing.py
@@ -47,6 +47,7 @@ class Nonni(Thing):
         self.name = 'Nonni'
         self.msg = 'Fyrir framan Nonna er fjöldi tómra glasa, hann umlar örlítið þegar þú ávarpar hann, þegar þú stjakar við honum snýr hann sér við og þú sérð að hann er mjög fullur. Hann segist geta hjálpað þér ef þú svarar einni spurningu.'
         self.question = 'Hver er munurinn á viskí og koníaki?'
+        super(Nonni, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         return '0' + self.msg
@@ -56,6 +57,7 @@ class Josefina(Thing):
         self.name = 'Jósefína'
         self.emptyhandedmsg = 'Jósefína slær þig utan undir fyrir ódæðissemi gærkvöldsins. Hún spyr þig hvernig þú vogir þér að koma til hennar tómhentur.'
         self.msg = 'Hún þakkar þér fyrir gærkvöldið og réttir þér nokkur ópíumlauf til að rétta þig af. Hún minnist á að þú hafir talað mikið um Einar gamla og ráðleggur þér að tala við hann'
+        super(Joseina, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         if earring:
@@ -69,6 +71,7 @@ class Einar(Thing):
         self.hungovermsg = 'Gamli maðurinn er gallharður bindindismaður, hann finnur áfengislyktina af þér og slær þig með þungum göngustaf.'
         self.msg = 'Einar muldrar eitthvað og lætur þig fá dularfullt kort sem leiðir þig að skóginum'
         self.hungover = True
+        super(Einar, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         if self.hungover:
@@ -82,6 +85,7 @@ class Kristjan(Thing):
     def __init__(self):
         self.name = 'Kristján'
         self.msg = 'setja skilaboð hér'
+        super(Kristjan, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         return self.msg
@@ -90,6 +94,7 @@ class Kona(Thing):
     def __init__(self):
         self.name = 'Kona'
         self.msg = 'setja skilaboð hér'
+        super(Kona, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         return self.msgS
@@ -98,6 +103,7 @@ class Skur(Thing):
     def __init__(self):
         self.name = 'Kona'
         self.msg = 'setja skilaboð hér'
+        super(Skur, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         return self.msg
@@ -106,6 +112,7 @@ class Villimadur(Thing):
     def __init__(self):
         self.name = 'villimaður'
         self.msg = 'setja skilaboð hér'
+        super(Villimadur, self).__init__(self.name)
 
     def interact(self, thing, hungover, key, earring, map):
         return self.msg
